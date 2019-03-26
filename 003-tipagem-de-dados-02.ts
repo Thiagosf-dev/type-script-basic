b0437c6c6524a236f20352dbf14070386b848996
Thiagosf-dev <thiagosf.dev@gmail.com>
2019-03-26 19:06:46 -0400

    Tipagem de dados parte 2

---
+++ 003-tipagem-de-dados-02.ts
@@ -0,0 +1,15 @@
+// TUPLA
+let campeaoBrasileiro: [string, number] = ["São Paulo", 2006];
+
+// ENUM
+enum Estado {
+  DESABILITADO = "Desabilitado",
+  HABILITADO = "Habilitado"
+}
+let meuEstado: Estado = Estado.HABILITADO;
+
+// tipo qualquer ou não especificado
+let isAnyType: any = "teste";
+
+// OBJECT
+let isObject: Object = {};
