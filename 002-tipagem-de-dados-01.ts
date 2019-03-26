33d39119fbb871254c147a05941b04620bec742d
Thiagosf-dev <thiagosf.dev@gmail.com>
2019-03-26 18:26:58 -0400

    Tipagem de dados parte 1

---
+++ 002-tipagem-de-dados-01.ts
@@ -0,0 +1,26 @@
+let isBooleano: boolean = true;
+
+let isString: string = "String";
+
+let isStringToo: string = "String";
+
+let isNumber: number = 1;
+
+let isBinaryNumber: number = 0b10011;
+
+let isOctalNumber: number = 0o13;
+
+let isHexadecimalNumber: number = 0xab13;
+
+let isFloatNumber: number = 1.0;
+
+let isArrayNumber: number[] = [1, 2, 3];
+
+let isArrayList: Array<number> = [1, 2, 3];
+
+// String expression
+let logradouro: string = "Avenida A";
+let numero: string = "124A";
+let cidade: string = "Cidade";
+let estado: string = "Estado";
+let fullEndereco: string = `Endereço: ${logradouro}, ${numero}, em ${cidade} - ${estado}`;
