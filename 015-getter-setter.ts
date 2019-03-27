4f592029f9fd8914c859da001f1c61d515e529e7
Thiagosf-dev <thiagosf.dev@gmail.com>
2019-03-26 21:09:16 -0400

    Getter e Setter

---
+++ 015-getter-setter.ts
@@ -0,0 +1,84 @@
+interface IAnimal {
+  nome: string;
+  idade: number;
+  estaVivo: boolean;
+
+  nascer(): void;
+  crescer(): void;
+  morrer(): void;
+}
+
+class Animal implements IAnimal {
+  
+  private _nome: string;
+  private _idade: number;
+  private _estaVivo: boolean;
+
+  // construtor
+  constructor(nome: string, idade?: number, vivo: boolean = true) {
+    this._nome = nome;
+    this._idade = idade;
+    this._estaVivo = vivo;
+  };
+
+  nascer(): void {
+    if(this._estaVivo) console.log("O animal nasceu");
+  }
+
+  crescer(): void {
+    this._idade++;
+   console.log("O animal cresceu");
+  }
+
+  morrer(): void {
+    this._estaVivo = false;
+   console.log("O animal morreu");
+  }
+  
+  //setters
+  set idade(novaIdade: number): void {
+    if(!novaIdade || novaIdade < 0) throw new Error("Idade inválida");
+
+    this._idade = novaIdade;
+    this._estaVivo = true;
+  }
+
+  set nome(novoNome: string): void {
+    if(!novoNome) throw new Error("Nome inválido");
+
+    this._nome = novoNome;
+    this._estaVivo = true;
+  }
+
+  // getters
+  get estaVivo(): boolean {
+    return this._estaVivo;
+  }
+
+  get idade(): number {
+    return this._idade;
+  }
+
+  get nome(): string {
+    return this._nome;
+  }
+
+}
+
+let cachorro: Animal = new Animal("Tótó", 3);
+cachorro.nascer();
+alert(cachorro.estaVivo);
+
+alert(cachorro.idade);
+cachorro.idade = 5;
+alert(cachorro.idade);
+
+alert(cachorro.nome);
+cachorro.nome = "Novo Nome do Tótó";
+alert(cachorro.nome);
+
+cachorro.crescer();
+alert(cachorro.idade);
+
+cachorro.morrer();
+alert(cachorro.estaVivo);
